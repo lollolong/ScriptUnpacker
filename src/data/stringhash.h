b951b2ba437578976fa40a31bab846cff210faf2
lollolong <65832054+lollolong@users.noreply.github.com>
2023-09-06 13:33:08 +0200

    missing include guard

--- src/data/stringhash.h
+++ src/data/stringhash.h
@@ -2,9 +2,14 @@
 //	data/stringhash.h
 //
 
+#ifndef _STRINGHASH_H_
+#define _STRINGHASH_H_
+
 namespace rage
 {
 	unsigned int atFinalizeHash(unsigned int hash);
 	unsigned int atPartialStringHash(const char* string);
 	unsigned int atStringHash(const char* string);
 }
+
+#endif // _STRINGHASH_H_
